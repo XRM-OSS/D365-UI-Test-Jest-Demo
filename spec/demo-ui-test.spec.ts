12482ed7b964997b50e45291d2ce312d98b0347c
Florian Kroenert <Florian.Kroenert@gmx.de>
2020-12-09 00:20:58 +0100

    First migration to playwright

--- spec/demo-ui-test.spec.ts
+++ spec/demo-ui-test.spec.ts
@@ -1,46 +1,29 @@
 import { XrmUiTest } from "d365-ui-test";
 import * as fs from "fs";
-import * as puppeteer from "puppeteer";
+import * as playwright from "playwright";
 import * as path from "path";
 
 const xrmTest = new XrmUiTest();
-let browser: puppeteer.Browser = null;
-let page: puppeteer.Page = null;
-
-const clearFiles = async (pathName: string, fileEndings: Array<string>): Promise<void> => {
-    const folderExists = await new Promise((resolve, reject) => 
-        fs.exists(pathName, (exists) => {
-            resolve(exists);
-        })
-    );
-
-    if (!folderExists) {
-        return;
-    }
-
-    return new Promise((resolve, reject) => 
-        fs.readdir(pathName, (err, files) => err 
-            ? reject(err) 
-            : resolve(files.filter(f => fileEndings.some(e => f.endsWith(e))).forEach(f => fs.unlinkSync(path.resolve(pathName, f))))));
-}
+let browser: playwright.Browser = null;
+let page: playwright.Page = null;
 
 describe("Basic operations UCI", () => {
     beforeAll(async () => {
         jest.setTimeout(60000);
-        await clearFiles("./reports", [".pdf'", ".pdf"]);
 
-        browser = await xrmTest.launch({
+        await xrmTest.launch("chromium", {
             headless: false,
-            args: ["--start-fullscreen"],
-            defaultViewport: null
+            args: ["--start-fullscreen"]
+        })
+        .then(([b, p]) => {
+            browser = b;
+            page = p;
         });
     });
 
     test("Start D365", async () => {
         const config = fs.readFileSync(path.join(__dirname, "../../settings.txt"), {encoding: 'utf-8'});
         const [url, user, password] = config.split(",");
-
-        page = await xrmTest.open(url, { userName: user, password: password });
     });
 
     test("Open new account form", async () => {
