d8a72e8eea31ccbd5d638e19ee8fa651a9af3cea
Florian Krönert <Florian.Kroenert@gmx.de>
2020-12-09 12:38:08 +0100

    Revert "First migration to playwright"
    This reverts commit 12482ed7b964997b50e45291d2ce312d98b0347c.

--- spec/demo-ui-test.spec.ts
+++ spec/demo-ui-test.spec.ts
@@ -1,29 +1,46 @@
 import { XrmUiTest } from "d365-ui-test";
 import * as fs from "fs";
-import * as playwright from "playwright";
+import * as puppeteer from "puppeteer";
 import * as path from "path";
 
 const xrmTest = new XrmUiTest();
-let browser: playwright.Browser = null;
-let page: playwright.Page = null;
+let browser: puppeteer.Browser = null;
+let page: puppeteer.Page = null;
+
+const clearFiles = async (pathName: string, fileEndings: Array<string>): Promise<void> => {
+    const folderExists = await new Promise((resolve, reject) => 
+        fs.exists(pathName, (exists) => {
+            resolve(exists);
+        })
+    );
+
+    if (!folderExists) {
+        return;
+    }
+
+    return new Promise((resolve, reject) => 
+        fs.readdir(pathName, (err, files) => err 
+            ? reject(err) 
+            : resolve(files.filter(f => fileEndings.some(e => f.endsWith(e))).forEach(f => fs.unlinkSync(path.resolve(pathName, f))))));
+}
 
 describe("Basic operations UCI", () => {
     beforeAll(async () => {
         jest.setTimeout(60000);
+        await clearFiles("./reports", [".pdf'", ".pdf"]);
 
-        await xrmTest.launch("chromium", {
+        browser = await xrmTest.launch({
             headless: false,
-            args: ["--start-fullscreen"]
-        })
-        .then(([b, p]) => {
-            browser = b;
-            page = p;
+            args: ["--start-fullscreen"],
+            defaultViewport: null
         });
     });
 
     test("Start D365", async () => {
         const config = fs.readFileSync(path.join(__dirname, "../../settings.txt"), {encoding: 'utf-8'});
         const [url, user, password] = config.split(",");
+
+        page = await xrmTest.open(url, { userName: user, password: password });
     });
 
     test("Open new account form", async () => {
