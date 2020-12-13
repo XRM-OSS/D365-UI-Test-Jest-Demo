47b113df440d145ddb26ee49eb8be8c8bf85fbbb
Florian Kroenert <Florian.Kroenert@gmx.de>
2020-12-13 03:42:38 +0100

    Upgraded to latest D365-UI-Test version

--- spec/demo-ui-test.spec.ts
+++ spec/demo-ui-test.spec.ts
@@ -5,18 +5,27 @@ import * as path from "path";
 
 const xrmTest = new XrmUiTest();
 let browser: playwright.Browser = null;
+let context: playwright.BrowserContext = null;
 let page: playwright.Page = null;
 
 describe("Basic operations UCI", () => {
-    beforeAll(async () => {
+    beforeAll(async() => {
         jest.setTimeout(60000);
 
         await xrmTest.launch("chromium", {
             headless: false,
-            args: ["--start-fullscreen"]
+            args: [
+                '--disable-setuid-sandbox',
+                '--disable-infobars',
+                '--start-fullscreen',
+                '--window-position=0,0',
+                '--window-size=1920,1080',
+                '--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"'
+            ]
         })
-        .then(([b, p]) => {
+        .then(([b, c, p]) => {
             browser = b;
+            context = c;
             page = p;
         });
     });
@@ -24,6 +33,8 @@ describe("Basic operations UCI", () => {
     test("Start D365", async () => {
         const config = fs.readFileSync(path.join(__dirname, "../../settings.txt"), {encoding: 'utf-8'});
         const [url, user, password] = config.split(",");
+
+        await xrmTest.open(url, { userName: user, password: password });
     });
 
     test("Open new account form", async () => {
